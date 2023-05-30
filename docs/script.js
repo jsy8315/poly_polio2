627a6a2925916df378ed739ee96698659dc6113f
jsy8315 <tdhitbtd@gmail.com>
2023-05-30 18:49:48 +0900

    flex구현

---
+++ docs/script.js
@@ -0,0 +1,44 @@
+<script>
+    fetch('data.json')
+          .then(response => response.json())
+          .then(data => {
+            const limitedData = data.slice(0, 4); // 처음 4개의 데이터만 사용
+    showInsuranceProducts(limitedData);
+          })
+          .catch(error => {
+        console.error('Error:', error);
+          });
+
+    function showInsuranceProducts(data) {
+          const cardDeck = document.querySelector('.card-deck');
+
+          data.forEach(product => {
+            const card = document.createElement('div');
+    card.classList.add('card');
+
+    const image = document.createElement('img');
+    image.classList.add('card-img-top');
+    image.src = product.image;
+    image.alt = 'Product Image';
+
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('card-body');
+
+    const title = document.createElement('h5');
+    title.classList.add('card-title');
+    title.textContent = product.title;
+
+    const description = document.createElement('p');
+    description.classList.add('card-text');
+    description.textContent = product.description;
+
+    cardBody.appendChild(title);
+    cardBody.appendChild(description);
+
+    card.appendChild(image);
+    card.appendChild(cardBody);
+
+    cardDeck.appendChild(card);
+          });
+        }
+</script>
--- src/circle.js
+++
@@ -1,6 +0,0 @@
-{
-    "imageData": {
-        "url": "https://thumb.mt.co.kr/06/2020/07/2020071418563658697_1.jpg/dims/optimize/",
-            "altText": "Example Image"
-    }
-}
